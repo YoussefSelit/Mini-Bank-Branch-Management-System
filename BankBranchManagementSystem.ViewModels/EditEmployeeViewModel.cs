94690eda12f642a568a9525dfcaacc467d0955f1
JoannaAprahamian <joanna.aprahamian@gmail.com>
2026-07-19 10:50:14 +0300

    added spaces to edit and create form tags

--- BankBranchManagementSystem.ViewModels/EditEmployeeViewModel.cs
+++ BankBranchManagementSystem.ViewModels/EditEmployeeViewModel.cs
@@ -4,32 +4,41 @@ namespace BankBranchManagementSystem.ViewModels
 {
     public class EditEmployeeViewModel
     {
+        [Display(Name = "Employee ID")]
         [Required]
         public int EmployeeId { get; set; }
 
+        [Display(Name = "First Name")]
         [Required]
         [StringLength(50)]
         public string EmployeeFirstName { get; set; } = null!;
 
+        [Display(Name = "Last Name")]
         [Required]
         [StringLength(50)]
         public string EmployeeLastName { get; set; } = null!;
 
+        [Display(Name = "Job Title")]
         [StringLength(50)]
         public string? EmployeeJobTitle { get; set; }
 
+        [Display(Name = "Phone Number")]
         [StringLength(50)]
         public string? EmployeePhone { get; set; }
 
+        [Display(Name = "Email")]
         [StringLength(50)]
         [EmailAddress(ErrorMessage = "Enter a valid email address.")]
         public string? EmployeeEmail { get; set; }
 
+        [Display(Name = "Hire Date")]
         public DateOnly? EmployeeHireDate { get; set; }
 
+        [Display(Name = "Branch Name")]
         [Required]
         public int EmployeeBranchId { get; set; }
 
+        [Display(Name = "Status")]
         [StringLength(10)]
         public string? EmploymentStatus { get; set; }
     }
